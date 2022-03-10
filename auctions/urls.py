b13b8688f89d4d8ef93dbc78e6baa1568a581a9e
Gareth Isherwood <34270547+g-ish@users.noreply.github.com>
2022-03-10 21:12:27 +0000

    .

--- auctions/urls.py
+++ auctions/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path("create_listing", views.create_listing, name="create_listing"),
     path('my_listings', views.view_user_listings, name="user_listings"),
     path('past_listings', views.view_past_listings, name="past_listings"),
-    path('listings/<int:listing_id>', views.view_listing, name="view_listing")
+    path('listings/<int:pk>', views.view_listing, name="view_listing")
 ]
